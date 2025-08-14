cdb6a0ede5ba1e1b9baa783bd115cfb23123d37d
Gersomsim <gersomhernandez@me.com>
2025-08-14 02:53:22 -0600

    Feat: Funcionalidad completa

--- main.go
+++ main.go
@@ -7,11 +7,17 @@ import (
 	"time"
 )
 
-
+const filename = "tasks.json"
 
 func main() {
 	var option int
 	var tasks []task.Task
+	tasks, err := task.LoadFromFile(filename)
+	if err != nil {
+		fmt.Printf("Error al cargar las tareas: %v\n", err)
+		time.Sleep(1 * time.Second)
+		return
+	}
 	for option != 4 {
 		fmt.Print("\033[H\033[2J")
 		fmt.Println("Bienvenido al gestor de tareas")
@@ -24,6 +30,14 @@ func main() {
 		option = optionSelected
 		task.Handler(option, &tasks)
 	}
+	err = task.SaveToFile(tasks, filename)
+	if err != nil {
+		fmt.Println("El trabajo se perderá")
+		fmt.Printf("Error al guardar las tareas: %v\n", err)
+		time.Sleep(1 * time.Second)
+		return
+	}
+
 	fmt.Println("Gracias por usar el gestor de tareas")
 
 
--- task/storage.go
+++ task/storage.go
@@ -1,5 +1,63 @@
 package task
 
-func Storage() {
-	
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"time"
+)
+
+const dir = "storage"
+
+func makeDir() {
+	err := os.Mkdir(dir, 0755)
+	if os.IsExist(err) {
+		return
+	}
+	if err != nil {
+		fmt.Printf("Error al crear el directorio: %v\n", err)
+		time.Sleep(1 * time.Second)
+		return
+	}
+}
+
+func SaveToFile(tasks []Task, filename string ) error {
+	fmt.Print("\033[H\033[2J")
+	makeDir()
+	fmt.Println("💾 Guardando tareas en el archivo...")
+
+	// creamos el archivo o lo sobreescribimos
+	file, err := os.Create(dir + "/" + filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	encoder := json.NewEncoder(file)
+	encoder.SetIndent("", "  ")
+	err = encoder.Encode(tasks)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func LoadFromFile(filename string) ([]Task, error) {
+	fmt.Print("\033[H\033[2J")
+	fmt.Println("Cargando tareas desde el archivo")
+	var tasks []Task
+	file, err := os.Open(dir + "/" + filename)
+
+	if err != nil {
+		if os.IsNotExist(err) {
+			return []Task{}, nil
+		}
+		return []Task{}, err
+	}
+	defer file.Close()
+	decoder := json.NewDecoder(file)
+	err = decoder.Decode(&tasks)
+	if err != nil {
+		return tasks, err
+	}
+	return tasks, nil
 }
